9aa5bf6995648e3aec6166a39edd275571417b7b
nathan-saygers <nathan.saygers@gmail.com>
2020-01-22 11:51:07 -0800

    initial commit

--- queries.sql
+++ queries.sql
@@ -2,8 +2,19 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.productName, c.categoryName
+FROM product as p
+JOIN category as c ON p.categoryId = c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+SELECT o.id, s.companyName
+FROM [order] as o
+JOIN shipper as s ON o.shipVIA = s.id
+WHERE o.orderDate < '2012-08-09';
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
