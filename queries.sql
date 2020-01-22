f9dae111a67301bfc57580143dbcfd2f811df483
nathan-saygers <nathan.saygers@gmail.com>
2020-01-22 12:13:57 -0800

    finished queries

--- queries.sql
+++ queries.sql
@@ -15,6 +15,19 @@ WHERE o.orderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT od.quantity, p.productName
+FROM orderDetail as od
+JOIN product as p 
+	ON od.productId = p.id
+WHERE od.orderId = 10251
+ORDER BY p.id asc;
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.id as "Order Number", c.companyName as "Customer Company Name", e.lastName as "Employee Name"
+FROM [order] as o
+JOIN employee as e
+	ON o.employeeID = e.id
+JOIN customer as c
+	ON o.customerID = c.id;
